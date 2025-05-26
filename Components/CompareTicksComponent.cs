5493eed542061e9ffff6e303129ebc0d6b33c6a4
Antoine <antoinemaesantoine@gmail.com>
2025-05-26 10:36:28 +0200

    compare ticks and timestamp component DRAFT

---
+++ Components/CompareTicksComponent.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Weasel.Components
+{
+    public class CompareTicksComponent : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the CompareTicks class.
+        /// </summary>
+        public CompareTicksComponent()
+          : base("Compare Time", "CmpT", "Is A more recent than B?", "Weasle", "Time")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddTimeParameter("Time A", "A", "First timestamp", GH_ParamAccess.item);
+            pManager.AddTimeParameter("Time B", "B", "Second timestamp", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("Is Newer", "N", "True if Time A is more recent than Time B", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            DateTime timeA = DateTime.MinValue;
+            DateTime timeB = DateTime.MinValue;
+
+            if (!DA.GetData(0, ref timeA)) return;
+            if (!DA.GetData(1, ref timeB)) return;
+
+            bool isNewer = timeA.Ticks > timeB.Ticks;
+            DA.SetData(0, isNewer);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("78364590-2713-4D7D-A91C-C222DD9830EC"); }
+        }
+    }
+}
---
+++ Components/TimestampComponent.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Weasel.Components
+{
+    public class TimestampComponent : GH_Component
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public TimestampComponent()
+          : base("TimeStamp", "TS",
+            "Get the timestamp, as an int, of when the input expired",
+            "Weasle", "Time")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            // Use the pManager object to register your input parameters.
+            // You can often supply default values when creating parameters.
+            // All parameters must have the correct access type. If you want 
+            // to import lists or trees of values, modify the ParamAccess flag.
+            pManager.AddGenericParameter("Source", "S", "Source to get Timestamp from", GH_ParamAccess.item);
+
+            // If you want to change properties of certain parameters, 
+            // you can use the pManager instance to access them by index:
+            //pManager[0].Optional = true;
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddTimeParameter("Timestamp", "T", "Current timestamp as integer", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            long ticks = DateTime.Now.Ticks;
+            DA.SetData(0, ticks);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("BEEBC809-6B7C-40A3-9C55-E79F50A59D55"); }
+        }
+    }
+}
